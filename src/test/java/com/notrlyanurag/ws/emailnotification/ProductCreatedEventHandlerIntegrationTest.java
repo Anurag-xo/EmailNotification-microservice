61f4d1d8dde031975e58673afe46aff4dbdb30f4
Anurag-xo <amnrag889@gmail.com>
2025-07-05 19:56:21 +0000

    test: create new test class

---
+++ src/test/java/com/notrlyanurag/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.notrlyanurag.ws.emailnotification;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@EmbeddedKafka
+@SpringBootTest(
+    properties = "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}")
+public class ProductCreatedEventHandlerIntegrationTest {}
